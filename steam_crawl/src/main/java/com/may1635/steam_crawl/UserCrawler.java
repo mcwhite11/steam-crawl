c0c4a276a91c683c7fbdf123d1dcbc18615495d6
Seth <sethg13@gmail.com>
2016-01-10 19:55:55 -0600

    UserCrawler get's friendslist

--- steam_crawl/src/main/java/com/may1635/steam_crawl/UserCrawler.java
+++ steam_crawl/src/main/java/com/may1635/steam_crawl/UserCrawler.java
@@ -1,31 +1,33 @@
 package com.may1635.steam_crawl;
 
-import com.may1635.domain.User;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
-import com.google.gson.JsonElement;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.reflect.TypeToken;
+import com.may1635.domain.User;
 
 public class UserCrawler {
 	
 	private static String key = "EE754BC31EC90048E3D53C788A3DC43D";
 	
 	public static void main(String[] args) {
+		
 		Stack<String> steamIDs = new Stack<String>();
 		steamIDs.push("76561197960435530"); // Robin Walker
-		fillStack(steamIDs);
+		gatherSteamIDs(steamIDs);
 		
 		HashMap<String, User> users = new HashMap<String, User>();
 		
@@ -84,14 +86,15 @@ public class UserCrawler {
 		}
 	}
 	
-	private static void fillStack(Stack<String> steamIDs) {
+	private static void gatherSteamIDs(Stack<String> steamIDs) {
 		ArrayList<String> friendsList = getFriendsList(steamIDs.pop());
 		
 		for(String friend : friendsList) {
 			steamIDs.push(friend);
 		}
 		
-		//TODO "recurse" through the stack and add friends friends friendlists to the stack...
+		//TODO "recurse" through the stack adding friend's friendlists to the stack...
+		// Still need a terminating case
 	}
 
 	public static ArrayList<String> getFriendsList(String steamID) {
@@ -106,24 +109,44 @@ public class UserCrawler {
 			// Connect to the URL using java's native library
 			HttpURLConnection request = (HttpURLConnection) url.openConnection();
 			request.connect();
-			
+
 			// Convert to a JSON object to print data
 			JsonParser jp = new JsonParser(); //from gson
-			JsonElement friendsEle = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-			JsonObject friendsObj = friendsEle.getAsJsonObject(); //May be an array, may be an object
-			String friends = friendsObj.get("steamid").getAsString(); //just grab the steamid
+			InputStreamReader stream = new InputStreamReader((InputStream) request.getContent());
+			JsonObject root = jp.parse(stream).getAsJsonObject();
+			JsonObject obj = root.get("friendslist").getAsJsonObject();
+			String friendsStr = obj.get("friends").toString();
 			
-			System.out.println(friendsObj);
-			System.out.println(friends);
-
-			//TODO add all friends not just one
-			//friendsList.add(friends);
+			Gson gson = new Gson();
+			Type type = new TypeToken<List<Friend>>(){}.getType();
+			ArrayList<Friend> friends = gson.fromJson(friendsStr, type);
+			
+			for(Friend friend : friends) {
+				friendsList.add(friend.getSteamID());
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		
 		return friendsList;
+	}
+	
+	// this wrapper class is needed to be able to pull the json objects
+	// requested from the steam api because that's how gson works
+	class Friend {
+		@SerializedName("steamid")
+		private String steamid;
+		@SerializedName("relationship")
+		private String relationship;
+		@SerializedName("friend_since")
+		private int friend_since;
+		
+		public Friend(){}
+		
+		public final String getSteamID() { return this.steamid; }
+		public final String getRelationship() { return this.relationship; }
+		public final int getFriendSince() { return this.friend_since; }
 	}
 	
 	public static Map<String, User> crawl(Stack<String> steamIDs) {
@@ -136,54 +159,58 @@ public class UserCrawler {
 				users.put(steamID, user);
 			}
 		}
+		
 		return users;
 	}
 	
 	public static User populateUser(String steamID) {
 		User user = new User();
 		
+		//http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=XXXXXXXXXXXXXXXXXXXXXXX&steamids=YYYYYYYYYYYYYYYYY
 		try {    
-			URL url = new URL("http://api.steampowered.com/ISteamUser/GetFriendList/v1/"
+			URL url = new URL("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/"
 					+ "?key=" + key
-					+ "&steamid=" + steamID
-					+ "&relationship=friend");
+					+ "&steamids=" + steamID);
 			
-		    // Connect to the URL using java's native library
-		    HttpURLConnection request = (HttpURLConnection) url.openConnection();
-		    request.connect();
+			// Connect to the URL using java's native library
+			HttpURLConnection request = (HttpURLConnection) url.openConnection();
+			request.connect();
 
-		    // Convert to a JSON object to print data
-		    JsonParser jp = new JsonParser(); //from gson
-		    JsonElement userEle = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
-		    JsonObject userObj = userEle.getAsJsonObject(); //May be an array, may be an object
-		    
-		    //TODO This really should be a constructor...
-			user.setSteamID(userObj.get("steamid").getAsString());
-			user.setPersonaName(userObj.get("personaname").getAsString());
-			user.setProfileURL(userObj.get("profileurl").getAsString());
+			// Convert to a JSON object to print data	
+			JsonParser jp = new JsonParser(); //from gson
+			InputStreamReader stream = new InputStreamReader((InputStream) request.getContent());
+			JsonObject root = jp.parse(stream).getAsJsonObject();
+			JsonObject response = root.get("response").getAsJsonObject();
+			JsonObject player = response.get("players").getAsJsonObject();
+			System.out.println(player);
+			
+		    // This really should be a constructor...
+			user.setSteamID(player.get("steamid").getAsString());
+			user.setPersonaName(player.get("personaname").getAsString());
+			user.setProfileURL(player.get("profileurl").getAsString());
 			user.setFriendsList(getFriendsList(steamID));
-			user.setAvatar(userObj.get("avatar").getAsString());
-			user.setAvatarMedium(userObj.get("avatarmedium").getAsString());
-			user.setAvatarFull(userObj.get("avatarfull").getAsString());
-			user.setPersonaState(userObj.get("personastate").getAsInt());
-			user.setVisibilityState(userObj.get("visibilitystate").getAsInt());
-			user.setProfileState(userObj.get("profilestate").getAsInt());
-			user.setLastLogOff(userObj.get("lastlogoff").getAsInt());
-			user.setCommentPermission(userObj.get("commentpermission").getAsInt());
-			user.setRealName(userObj.get("realname").getAsString());
-			user.setPrimaryClanID(userObj.get("primaryclanid").getAsString());
-			user.setTimeCreated(userObj.get("timecreated").getAsString());
-			user.setGameID(userObj.get("gameid").getAsString());
-			user.setGameServerIP(userObj.get("gameserverip").getAsString());
-			user.setGameExtraInfo(userObj.get("gameextrainfo").getAsString());
-			user.setLocCountryCode(userObj.get("loccountrycode").getAsString());
-			user.setLocStateCode(userObj.get("locstatecode").getAsString());
-			user.setLocCityID(userObj.get("loccityid").getAsString());
-
+			user.setAvatar(player.get("avatar").getAsString());
+			user.setAvatarMedium(player.get("avatarmedium").getAsString());
+			user.setAvatarFull(player.get("avatarfull").getAsString());
+			user.setPersonaState(player.get("personastate").getAsInt());
+			user.setVisibilityState(player.get("visibilitystate").getAsInt());
+			user.setProfileState(player.get("profilestate").getAsInt());
+			user.setLastLogOff(player.get("lastlogoff").getAsInt());
+			user.setCommentPermission(player.get("commentpermission").getAsInt());
+			user.setRealName(player.get("realname").getAsString());
+			user.setPrimaryClanID(player.get("primaryclanid").getAsString());
+			user.setTimeCreated(player.get("timecreated").getAsString());
+			user.setGameID(player.get("gameid").getAsString());
+			user.setGameServerIP(player.get("gameserverip").getAsString());
+			user.setGameExtraInfo(player.get("gameextrainfo").getAsString());
+			user.setLocCountryCode(player.get("loccountrycode").getAsString());
+			user.setLocStateCode(player.get("locstatecode").getAsString());
+			user.setLocCityID(player.get("loccityid").getAsString());
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		
 		return user;
 	}
 }
